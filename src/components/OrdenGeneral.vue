ee997d1a3573061e3611442a608aa1382035a70e
anasu <misushima@gmail.com>
2020-09-23 20:03:34 -0300

    Se arregla la lógica de las componentes

---
+++ src/components/OrdenGeneral.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <v-list class="list--maxwidth">
+      <v-list-item>
+        <v-list-item-content>N Orden</v-list-item-content>
+        <v-list-item-content>{{ this.contenidoT1.num_orden }}</v-list-item-content>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-content>Monto Orden</v-list-item-content>
+        <v-list-item-content>{{ this.contenidoT1.monto }}</v-list-item-content>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-content>Cant. Productos</v-list-item-content>
+        <v-list-item-content>{{ this.contenidoT1.cant_productos }}</v-list-item-content>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-content>Fecha Entrega</v-list-item-content>
+        <v-list-item-content>{{ this.date }}</v-list-item-content>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-content>Vendedor</v-list-item-content>
+        <v-list-item-content>{{ this.contenidoT1.vendedor }}</v-list-item-content>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-content>Estado</v-list-item-content>
+        <v-list-item-content>{{ this.contenidoT1.estado }}</v-list-item-content>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-content>Avance Preparación</v-list-item-content>
+        <v-list-item-content>
+          <v-progress-linear
+            background-color="info"
+            color="grey lighten-4"
+            :value="this.advancePCT"
+            height="10"
+            rounded
+            class="border-grey"
+          ></v-progress-linear>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tabs',
+  props: {
+    contenidoGeneral: Object,
+    dateFixed: String, 
+    advanceFixed: Number
+  },
+  data()
+  {
+    return {
+      tab: null,
+      contenidoT1: this.contenidoGeneral,
+      date: this.dateFixed,
+      advancePCT: this.advanceFixed,
+    }
+  },
+  computed: {
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.border-grey {
+  border: solid 1px grey;
+}
+.list--maxwidth {
+  max-width: 450px;
+}
+</style>
--- src/views/Orden.vue
+++ src/views/Orden.vue
@@ -3,27 +3,46 @@
     <!-- breadcrumb -->
     <h1>Monitor de Órdenes</h1>
 
-    <tabs
-      :tabNames="this.tabNames"
-      :contentTab1="this.orden"
-      :advancePercentage="this.getFixedAdvance"
-      :dateFixed="this.getFixedDate"
-    ></tabs>
+    <v-tabs v-model="tab">
+      <v-tab
+        v-for="item in tabNames"
+        :key="item"
+      >
+        {{ item }}
+      </v-tab>
+    </v-tabs>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item>
+        <orden-general
+          :contenidoGeneral="this.orden"
+          :advanceFixed="this.getFixedAdvance"
+          :dateFixed="this.getFixedDate"
+        ></orden-general>
+      </v-tab-item>
+      <v-tab-item>
+        b
+      </v-tab-item>
+      <v-tab-item>
+        c
+      </v-tab-item>
+    </v-tabs-items>
   </div>
 </template>
 
 <script>
-import Tabs from '@/components/Tabs'
+import OrdenGeneral from '@/components/OrdenGeneral'
 import { mapState, mapGetters } from 'vuex'
 
 export default {
   name: 'DetalleOrden',
   components: {
-    Tabs,
+    OrdenGeneral,
   },
   data() 
   {
     return {
+      tab: null,
       tabNames: [
         'General',
         'Datos Cliente',
